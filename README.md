02efc53b58cb1efb1a963b144abc6faeb10123b2
Sabrina Templeton <sabrina@sabrinas-mbp.middlebury.edu>
2021-10-13 16:27:49 -0400

    some progress towards api

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 An app for the deduction of Arabic sentences
 
 References:
+(Swift)
+https://www.youtube.com/watch?v=sqo844saoC4
+https://www.youtube.com/watch?v=joy9hu06J7g
--- xCode_project/qT3/qT3/ViewController.swift
+++ xCode_project/qT3/qT3/ViewController.swift
@@ -26,14 +26,60 @@ class ViewController: UIViewController {
         label = UILabel(frame: CGRect(x: 100, y: 200, width: 220, height: 50))
         label.text = "text"
         view.addSubview(label)
+        
+        let url = "http://api.sunrise-sunset.org/json?date=2020-01-01&lat=-74.0060&lng=40.7128&formatted=0"
+        getData(from: url)
     }
     
     @IBAction func buttonTapped(){
         field.resignFirstResponder()
     }
 
+    private func getData(from url: String){
+        let task = URLSession.shared.dataTask(with: URL(string: url)!,completionHandler: {data, response, error in
+            guard let data = data, error == nil else {
+                print("something went wrong")
+                return
+            }
+            //have data
+            var result: Response?
+            do{
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch{
+                print("failed to convert \(error.localizedDescription)")
+            }
+            guard let json = result else {
+                return
+            }
+            print(json.status)
+            print(json.results.sunrise)
+            print(json.results.sunset)
+             
+        })
+        task.resume()
+    }
+    struct Response: Codable {
+        let results: MyResult
+        let status: String
+    }
+    struct MyResult: Codable{
+        let sunrise: String
+        let sunset: String
+        let solar_noon: String
+        let day_length: Int
+        let civil_twilight_begin: String
+        let civil_twilight_end: String
+        let nautical_twilight_begin: String
+        let nautical_twilight_end: String
+        let astronomical_twilight_begin: String
+        let astronomical_twilight_end: String
+        
+    }
 
 }
+
+
 
 extension ViewController: UITextFieldDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
