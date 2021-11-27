a5536a5a0f4ebd8296f6a2d36f734a967bb51863
Sabrina Templeton <sabrina@Sabrinas-MacBook-Pro.local>
2021-11-27 14:50:54 -0500

    a couple more tests

--- api_code/tests.py
+++ api_code/tests.py
@@ -23,7 +23,31 @@ class Features:
         self.mood = mood
         self.prefix_count = 0
         self.suffix_count = 0
+        self.hash_string = self.calc_hash_string()
+    
+    def __eq__(self, o: object) -> bool:
+        return self.tense == o.tense and self.number == o.number and self.gender == o.gender and self.person == o.person and self.mood == o.mood
+    def __hash__(self) -> int:
+        return hash(self.hash_string)
+    def calc_hash_string(self):
+        """
+        pretty much an 'encode features' method
+        """
+        hash_string = ""
+        if self.tense == "past":
+            hash_string += "p"
+        elif self.tense == "present":
+            hash_string += "r"
+        
+        hash_string += str(self.number)
 
+        hash_string += self.gender[0]
+
+        hash_string += str(self.person)
+
+        hash_string += self.mood[0]
+
+        return hash_string
 def test_form1_check():
     basic = verb_base_algorithm.make_word("فعل")
     hollow = verb_base_algorithm.make_word("نام")
@@ -156,7 +180,7 @@ def test_form10_check():
     assert verb_base_algorithm.check_vii(basic) == False
     assert verb_base_algorithm.check_viii(basic) == True
 
-    
+   
 def test_which_form_check_if():
     # basic = verb_base_algorithm.make_word("فعّل")
     # assert verb_base_algorithm.which_form(basic) == "Form II", basic
@@ -221,16 +245,74 @@ def test_correct_form():
     assert hamsa.root == "ءكل"
     
 
+def test_deconjugate():
+    #  test we form
+    first_plural = verb_base_algorithm.make_word("نفعل")
+    first_plural_feature1 = verb_base_algorithm.decode_features("r1n3i")
+    first_plural_feature2 = verb_base_algorithm.decode_features("r1n3j")
+    first_plural_feature3 = verb_base_algorithm.decode_features("r1n3s")
+    first_plural = verb_base_algorithm.deconjugate(first_plural)
+    assert first_plural_feature1 in first_plural.features
+    assert first_plural_feature2 in first_plural.features
+    assert first_plural_feature3 in first_plural.features
+    assert len(first_plural.features) == 3
+
+    first_sing = verb_base_algorithm.make_word("أفعل")
+    first_sing_feature1 = verb_base_algorithm.decode_features("r1n1i")
+    first_sing_feature2 = verb_base_algorithm.decode_features("r1n1j")
+    first_sing_feature3 = verb_base_algorithm.decode_features("r1n1s")
+    first_sing = verb_base_algorithm.deconjugate(first_sing)
+    assert first_sing_feature1 in first_sing.features
+    assert first_sing_feature2 in first_sing.features
+    assert first_sing_feature3 in first_sing.features
+    assert len(first_plural.features) == 3
+
+    #checks first person past tense, aka feminine past singular and second past singular forms
+    first_past = verb_base_algorithm.make_word("فعلت")
+    first_past_feature1 = verb_base_algorithm.decode_features("p1n1n")
+    first_past_feature2 = verb_base_algorithm.decode_features("p3f1n")
+    first_past_feature3 = verb_base_algorithm.decode_features("p2f1n")
+    first_past_feature4 = verb_base_algorithm.decode_features("p2m1n")
+    first_past = verb_base_algorithm.deconjugate(first_past)
+    assert first_past_feature1 in first_past.features
+    assert first_past_feature2 in first_past.features
+    assert first_past_feature3 in first_past.features
+    assert first_past_feature4 in first_past.features
+
+    assert len(first_past.features) == 4
 
 
+    # test first person secoond form
+    # not sure why this test isn't working rn, will come back to it
+    # second_person = verb_base_algorithm.make_word("تفعلين")
+    # #second_feature1 = verb_base_algorithm.decode_features("r3f1i")
+    # second_feature2 = verb_base_algorithm.decode_features("r2f1i")
+    # #second_feature3 = verb_base_algorithm.decode_features("r3f1j")
+    # #second_feature4 = verb_base_algorithm.decode_features("r2f1j")
+    # #second_feature5 = verb_base_algorithm.decode_features("r3f1s")
+    # #second_feature6 = verb_base_algorithm.decode_features("r2f1s")
 
+    # #assert second_feature1 in second_person.features
+    # assert second_feature2 in second_person.features
+    # #assert second_feature3 in second_person.features
+    # #assert second_feature4 in second_person.features
+    # #assert second_feature5 in second_person.features
+    # #assert second_feature6 in second_person.features
 
+    # assert len(second_person.features) == 6
 #     form, word = verb_base_algorithm.which_form(verb_base_algorithm.deconjugate(basic))
 #     assert word.root == "فعل"
 #     form, word = verb_base_algorithm.which_form(verb_base_algorithm.deconjugate(sample))
     
 #     #shoot we have another ambiguity between form v and form 2
 
+def test_new_hash():
+    #tests feature equality
+    f1 = verb_base_algorithm.decode_features("p1n1n")
+    f1a = verb_base_algorithm.decode_features("p1n1n")
+    f2 = verb_base_algorithm.decode_features("p1f1n")
+    assert f1 == f1a
+    assert f1 != f2
 
 # def test_defective():
 #     type1 = verb_base_algorithm.make_word()
