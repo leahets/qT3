272c073bc8108dad383d0d39ed8b3eff0a1c9bde
leahets <91686368+leahets@users.noreply.github.com>
2021-12-01 16:17:25 -0500

    fixed conflicting changes

--- api_code/tests.py
+++ api_code/tests.py
@@ -1,5 +1,6 @@
 import verb_base_algorithm
 
+
 class Word:
     def __init__(self, raw_text):
         self.raw_text = raw_text
@@ -24,11 +25,13 @@ class Features:
         self.prefix_count = 0
         self.suffix_count = 0
         self.hash_string = self.calc_hash_string()
-    
-    def __eq__(self, o: object) -> bool:
+
+    def __eq__(self, o) -> bool:
         return self.tense == o.tense and self.number == o.number and self.gender == o.gender and self.person == o.person and self.mood == o.mood
+
     def __hash__(self) -> int:
         return hash(self.hash_string)
+
     def calc_hash_string(self):
         """
         pretty much an 'encode features' method
@@ -38,7 +41,7 @@ class Features:
             hash_string += "p"
         elif self.tense == "present":
             hash_string += "r"
-        
+
         hash_string += str(self.number)
 
         hash_string += self.gender[0]
@@ -48,12 +51,14 @@ class Features:
         hash_string += self.mood[0]
 
         return hash_string
+
+
 def test_form1_check():
     basic = verb_base_algorithm.make_word("فعل")
     hollow = verb_base_algorithm.make_word("نام")
     defective = verb_base_algorithm.make_word("بقع")
-    formv = verb_base_algorithm.make_word("تعرّف") 
-    form2 = verb_base_algorithm.make_word("درّس") 
+    formv = verb_base_algorithm.make_word("تعرّف")
+    form2 = verb_base_algorithm.make_word("درّس")
 
     assert verb_base_algorithm.check_i(basic) == True
     assert verb_base_algorithm.check_i(hollow) == True
@@ -74,6 +79,7 @@ def test_form1_check():
 
     # assert verb_base_algorithm.check_ii(bacis) == False
 
+
 def test_form2_check():
     basic = verb_base_algorithm.make_word("فعّل")
     assert verb_base_algorithm.check_ii(basic) == True
@@ -87,11 +93,11 @@ def test_form2_check():
     assert verb_base_algorithm.check_viii(basic) == False
     assert verb_base_algorithm.check_x(basic) == False
 
+
 def test_form3_check():
     basic = verb_base_algorithm.make_word("فاعل")
 
     assert verb_base_algorithm.check_iii(basic) == True
-
 
     assert verb_base_algorithm.check_i(basic) == False
     assert verb_base_algorithm.check_ii(basic) == False
@@ -101,6 +107,7 @@ def test_form3_check():
     assert verb_base_algorithm.check_vii(basic) == False
     assert verb_base_algorithm.check_viii(basic) == False
     assert verb_base_algorithm.check_x(basic) == False
+
 
 def test_form4_check():
     basic = verb_base_algorithm.make_word("أفعل")
@@ -115,6 +122,7 @@ def test_form4_check():
     assert verb_base_algorithm.check_viii(basic) == False
     assert verb_base_algorithm.check_x(basic) == False
 
+
 def test_form5_check():
     basic = verb_base_algorithm.make_word("تفعّل")
     assert verb_base_algorithm.check_v(basic) == True
@@ -127,6 +135,7 @@ def test_form5_check():
     assert verb_base_algorithm.check_vii(basic) == False
     assert verb_base_algorithm.check_viii(basic) == False
     assert verb_base_algorithm.check_x(basic) == False
+
 
 def test_form6_check():
     basic = verb_base_algorithm.make_word("تفاعل")
@@ -141,6 +150,7 @@ def test_form6_check():
     assert verb_base_algorithm.check_viii(basic) == False
     assert verb_base_algorithm.check_x(basic) == False
 
+
 def test_form7_check():
     basic = verb_base_algorithm.make_word("انفعل")
     assert verb_base_algorithm.check_vii(basic) == True
@@ -153,6 +163,7 @@ def test_form7_check():
     assert verb_base_algorithm.check_vi(basic) == False
     assert verb_base_algorithm.check_viii(basic) == False
     assert verb_base_algorithm.check_x(basic) == False
+
 
 def test_form8_check():
     basic = verb_base_algorithm.make_word("افتعل")
@@ -167,6 +178,7 @@ def test_form8_check():
     assert verb_base_algorithm.check_vii(basic) == False
     assert verb_base_algorithm.check_x(basic) == False
 
+
 def test_form10_check():
     basic = verb_base_algorithm.make_word("استعمل")
     assert verb_base_algorithm.check_x(basic) == True
@@ -180,12 +192,13 @@ def test_form10_check():
     assert verb_base_algorithm.check_vii(basic) == False
     assert verb_base_algorithm.check_viii(basic) == True
 
-   
+
 def test_which_form_check_if():
     # basic = verb_base_algorithm.make_word("فعّل")
     # assert verb_base_algorithm.which_form(basic) == "Form II", basic
     gibberish = verb_base_algorithm.make_word("ثتصصنيبيت")
     assert verb_base_algorithm.which_form(gibberish) == "Not a verb", gibberish
+
 
 def test_root():
     basic = verb_base_algorithm.make_word("فعل")
@@ -193,7 +206,6 @@ def test_root():
     sample = verb_base_algorithm.make_word("يلعب")
 
     tricky = verb_base_algorithm.make_word("نام")
-
 
     basic1 = verb_base_algorithm.deconjugate(basic)
     basic2 = verb_base_algorithm.strip_fixes(basic1)
@@ -214,23 +226,28 @@ def test_root():
 def test_correct_form_basic():
     pass
 
+
 def test_correct_form():
-    #tests form eight and form ten where the leading alif is dropped in all present tense conjugations
+    # tests form eight and form ten where the leading alif is dropped in all present tense conjugations
     leading_alif_dropped = verb_base_algorithm.make_word("يستعملون")
 
-    leading_alif_dropped = verb_base_algorithm.deconjugate(leading_alif_dropped)
-    leading_alif_dropped = verb_base_algorithm.strip_fixes(leading_alif_dropped)
-    leading_form, leading_alif_dropped = verb_base_algorithm.which_form(leading_alif_dropped)
-
+    leading_alif_dropped = verb_base_algorithm.deconjugate(
+        leading_alif_dropped)
+    leading_alif_dropped = verb_base_algorithm.strip_fixes(
+        leading_alif_dropped)
+    leading_form, leading_alif_dropped = verb_base_algorithm.which_form(
+        leading_alif_dropped)
 
     assert leading_form == "Form X"
 
     leading_alif_dropped1 = verb_base_algorithm.make_word("نقتتل")
 
-    leading_alif_dropped1 = verb_base_algorithm.deconjugate(leading_alif_dropped1)
-    leading_alif_dropped1 = verb_base_algorithm.strip_fixes(leading_alif_dropped1)
-    leading_form1, leading_alif_dropped1 = verb_base_algorithm.which_form(leading_alif_dropped1)
-
+    leading_alif_dropped1 = verb_base_algorithm.deconjugate(
+        leading_alif_dropped1)
+    leading_alif_dropped1 = verb_base_algorithm.strip_fixes(
+        leading_alif_dropped1)
+    leading_form1, leading_alif_dropped1 = verb_base_algorithm.which_form(
+        leading_alif_dropped1)
 
     assert leading_form1 == "Form VIII"
     # test hamsated verbs like "to eat" where first person form creates double hamsa
@@ -243,7 +260,7 @@ def test_correct_form():
 
     assert hamsa_form == "Form I"
     assert hamsa.root == "ءكل"
-    
+
 
 def test_deconjugate():
     #  test we form
@@ -267,7 +284,7 @@ def test_deconjugate():
     assert first_sing_feature3 in first_sing.features
     assert len(first_plural.features) == 3
 
-    #checks first person past tense, aka feminine past singular and second past singular forms
+    # checks first person past tense, aka feminine past singular and second past singular forms
     first_past = verb_base_algorithm.make_word("فعلت")
     first_past_feature1 = verb_base_algorithm.decode_features("p1n1n")
     first_past_feature2 = verb_base_algorithm.decode_features("p3f1n")
@@ -280,7 +297,6 @@ def test_deconjugate():
     assert first_past_feature4 in first_past.features
 
     assert len(first_past.features) == 4
-
 
     # test first person secoond form
     # not sure why this test isn't working rn, will come back to it
@@ -303,11 +319,12 @@ def test_deconjugate():
 #     form, word = verb_base_algorithm.which_form(verb_base_algorithm.deconjugate(basic))
 #     assert word.root == "فعل"
 #     form, word = verb_base_algorithm.which_form(verb_base_algorithm.deconjugate(sample))
-    
+
 #     #shoot we have another ambiguity between form v and form 2
 
+
 def test_new_hash():
-    #tests feature equality
+    # tests feature equality
     f1 = verb_base_algorithm.decode_features("p1n1n")
     f1a = verb_base_algorithm.decode_features("p1n1n")
     f2 = verb_base_algorithm.decode_features("p1f1n")
@@ -316,9 +333,10 @@ def test_new_hash():
 
 # def test_defective():
 #     type1 = verb_base_algorithm.make_word()
-    
+
+
 #     assert verb_base_algorithm.deconjugate()
 if __name__ == "__main__":
     import pytest
     pytest.main([__file__])
-    #test_form1_check()
+    # test_form1_check()
